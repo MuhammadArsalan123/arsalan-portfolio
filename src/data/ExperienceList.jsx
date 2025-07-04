3411a1c9512dcd2011bcb3af3cea380a256e070b
muhammadarsalan1432 <muhammadarsalan1432@gmail.com>
2025-07-04 16:22:27 +0500

    2022 to 2023

--- src/data/ExperienceList.jsx
+++ src/data/ExperienceList.jsx
@@ -4,7 +4,7 @@ export const ExperiencesList = {
       {
         name: "Catalyic Security",
         description:
-          "Since 2022, I have developed dynamic, cross-browser compatible web pages using HTML5, CSS3, jQuery, Bootstrap, and JavaScript. I expanded features, optimized code, and streamlined processes to boost operational efficiency and user engagement. I also authored technical documentation, shared knowledge within the development team, met project targets, adhered to industry best practices, and actively participated in code reviews.",
+          "Since 2023, I have developed dynamic, cross-browser compatible web pages using HTML5, CSS3, jQuery, Bootstrap, and JavaScript. I expanded features, optimized code, and streamlined processes to boost operational efficiency and user engagement. I also authored technical documentation, shared knowledge within the development team, met project targets, adhered to industry best practices, and actively participated in code reviews.",
         linkedInUrl: "https://www.linkedin.com/company/catalyic-tech/",
       },
     ],
